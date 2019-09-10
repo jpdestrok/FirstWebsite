d3994499d9524a4e65c6b2ca6c6968df50330290
jpdestrok <51759471+jpdestrok@users.noreply.github.com>
2019-09-10 16:11:19 -0700

    adding jquery(unfinished)

---
+++ resources/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    // For sticky navigation
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+            offset: '75%;'
+        });
+
+    // Scroll on button
+    $('.js--scroll-to-plan').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+});
